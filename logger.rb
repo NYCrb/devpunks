177def12dae62503157d5634523194d6567fec74
Snuggs <rashaunstovall@gmail.com>
2019-05-16 23:16:50 -0400

    Update to use logger

--- logger.rb
+++ logger.rb
@@ -10,6 +10,6 @@ logger = CustomLogger.new (STDOUT)
 
 puts ActiveSupport::Logger.broadcast(logger)
 
-seo_logger.debug "Debug level"
-seo_logger.info "Info level"
-seo_logger.error "Error level"
+logger.debug "Debug level"
+logger.info "Info level"
+logger.error "Error level"
